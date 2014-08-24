fdd4620f22f03cbba893c8fa50e8bd9b1773d162
Ljubomir Francuski <ljubomir.francuski@gmail.com>
2014-08-24 12:04:41 -0400

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,36 @@
+#Loading data...
+#test data
+testXData<-read.table(".\\UCI HAR Dataset\\test\\X_test.txt")
+testYData<-read.table(".\\UCI HAR Dataset\\test\\y_test.txt")
+subjectsTest<-read.table(".\\UCI HAR Dataset\\test\\subject_test.txt")
+#train data
+trainXData<-read.table(".\\UCI HAR Dataset\\train\\X_train.txt")
+trainYData<-read.table(".\\UCI HAR Dataset\\train\\y_train.txt")
+subjectsTrain<-read.table(".\\UCI HAR Dataset\\train\\subject_train.txt")
+#Features
+features<-read.table(".\\UCI HAR Dataset\\features.txt")
+#Labels
+activityLabels<-read.table(".\\UCI HAR Dataset\\activity_labels.txt")
+
+finalDataSet<-rbind(testXData,trainXData)
+
+#step2
+
+matches<-grepl("(mean*())|(std*())",features$V2)
+colData<-features$V1[matches]
+tidyDS<-finalDataSet[,colData]
+tidyDS<-cbind(tidyDS,cbind(rbind(subjectsTest,subjectsTrain),rbind(testYData,trainYData)))
+
+#step3
+tidyDS[,81]<-as.factor(tidyDS[,81])
+levels(tidyDS[,81])<-activityLabels$V2
+
+#step4
+names(tidyDS)<-c(as.character(features$V2[matches]),"Subject","Activity")
+
+#step5
+aggData<-aggregate(tidyDS[,1:79], by=list(Subject=tidyDS$Subject,Activity=tidyDS$Activity),FUN=mean,na.rm=TRUE)
+
+#Write table
+write.table(aggData,"tidy.txt",col.names=FALSE)
+
